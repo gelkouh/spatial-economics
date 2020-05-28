d12c14c455fddf722c7d9b8d9808f988d91e2b12
gelkouh <gelkouh@uchicago.edu>
2020-05-28 17:13:52 -0400

    docs and scripts

--- scripts/calfires_oeconcohort.R
+++ scripts/calfires_oeconcohort.R
@@ -30,6 +30,26 @@ if (!require(reshape2)) install.packages('reshape2')
 library(reshape2)
 if (!require(tidyr)) install.packages('tidyr')
 library(tidyr)
+if (!require(rgdal)) install.packages('rgdal')
+library(rgdal)
+if (!require(rgeos)) install.packages('rgeos')
+library(rgeos)
+if (!require(tmap)) install.packages('tmap')
+library(tmap)
+if (!require(leaflet)) install.packages('leaflet')
+library(leaflet)
+if (!require(RColorBrewer)) install.packages('RColorBrewer')
+library(RColorBrewer)
+if (!require(sp)) install.packages('sp')
+library(sp)
+if (!require(raster)) install.packages('raster')
+library(raster)
+if (!require(adehabitatHR)) install.packages('adehabitatHR')
+library(adehabitatHR)
+if (!require(tmaptools)) install.packages('tmaptools')
+library(tmaptools)
+if (!require(gridExtra)) install.packages('gridExtra')
+library(gridExtra)
 
 #-----------------------------------
 # Loading and Cleaning the Data
@@ -39,11 +59,16 @@ library(tidyr)
 # We are trying to get a panel data dataframe that has the percentage burned 
 # by forest fires of each California county in each year from 1990-2000
 # with economic data about that county from a county profile supplied by the BEA
+# and the housing quantities provided by Zillow
 
 # General code for importing data: 
 # df <- read_XXX(file.path(ddir, "Folder", "filename.ext"))
 
+# Will be used to clean up county names below
 delete_county_word <- c(" County", ", CA")
+
+# County shapefiles
+counties_shapes <- readOGR(file.path(ddir,"CA_Counties"))
 
 # Forest fire intersections calculated using QGIS
 intersect_1990_df <- read_csv(file.path(ddir,"Analysis Files", "Intersect2.csv"))
@@ -62,8 +87,8 @@ econ_indicators_df <- read_csv(file.path(ddir,"Analysis Files", "CAINC30", "CAIN
   dplyr::rename("COUNTY" = "GeoName") %>%
   dplyr::rename("ECON_INDICATOR" = "Description") %>% 
   dplyr::rename("INDICATOR_UNITS" = "Unit") %>%
-  select(-c("2012","2013","2014","2015","2016","2017","2018")) %>%
-  select(-c("GeoFIPS"))
+  dplyr::select(-c("2012","2013","2014","2015","2016","2017","2018")) %>%
+  dplyr::select(-c("GeoFIPS"))
 econ_indicators_df$COUNTY <- removeWords(econ_indicators_df$COUNTY,delete_county_word)
 econ_indicators_df <- econ_indicators_df %>%
   reshape2::melt(id=c("COUNTY","ECON_INDICATOR","INDICATOR_UNITS")) %>%
@@ -91,11 +116,11 @@ zillow_county_for_sale_listings_df <- read_csv(file.path(ddir,"Analysis Files", 
   filter(State == "CA")
 zillow_county_for_sale_listings_df$COUNTY <- removeWords(zillow_county_for_sale_listings_df$COUNTY,delete_county_word)
 zillow_county_for_sale_listings_df <- zillow_county_for_sale_listings_df %>%
-  select(-contains(c("-02","-03","-04","-05","-06","-07","-08","-09","-10","-11","-01"))) %>%
-  select(-contains(c("2012","2013","2014","2015","2016","2017","2018","2019","2020")))
+  dplyr::select(-contains(c("-02","-03","-04","-05","-06","-07","-08","-09","-10","-11","-01"))) %>%
+  dplyr::select(-contains(c("2012","2013","2014","2015","2016","2017","2018","2019","2020")))
 colnames(zillow_county_for_sale_listings_df) <- c(1,2,"COUNTY",1990:2011)
 zillow_county_for_sale_listings_df <- zillow_county_for_sale_listings_df %>%
-  select(c("COUNTY","1996","1997","1998","1999","2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011")) %>%
+  dplyr::select(c("COUNTY","1996","1997","1998","1999","2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011")) %>%
   reshape2::melt(id=c("COUNTY")) %>%
   dplyr::rename("YEAR"="variable") %>%
   dplyr::rename("ZILLOW_FOR_SALE_LISTINGS_QUANTITY_DEC31"="value")
@@ -111,13 +136,13 @@ intersect_df <- intersect_df %>%
   dplyr::rename("INTERSECT_SHAPE_AREA" = "INT_AREA") %>%
   dplyr::rename("ZCTA_2019" = "ZCTA5CE10") %>%
   dplyr::rename("YEAR" = "YEAR_") %>%
-  select(-c("GEOID10","ALAND10","AWATER10"))
+  dplyr::select(-c("GEOID10","ALAND10","AWATER10"))
 zcta_2019_df <- zcta_2019_df %>%
   dplyr::rename("ZCTA_SHAPE_AREA" = "ZCTA_AREA2") %>%
   dplyr::rename("ZCTA_2019" = "ZCTA5CE10")
 zcta_2019_df <- merge(zcta_2019_df,zipcodes_by_state_df,by="ZCTA_2019",all.x=TRUE) %>%
   filter(state_id == "CA") %>%
-  select(c("ZCTA_2019","ZCTA_SHAPE_AREA","POPULATION"="population","COUNTY"="county_name"))
+  dplyr::select(c("ZCTA_2019","ZCTA_SHAPE_AREA","POPULATION"="population","COUNTY"="county_name"))
 county_pop_df$COUNTY <- removeWords(county_pop_df$COUNTY,delete_county_word)
 zcta_2019_df <- merge(zcta_2019_df,county_pop_df,by="COUNTY",all.x = TRUE)
 
@@ -149,7 +174,7 @@ county_weighted_percentage_df <- county_weighted_percentage_df[!is.na(county_wei
 
 # Add economic indicators and include only one observation per year per county for panel dataframe to use in analysis
 panel_df <- merge(county_weighted_percentage_df,econ_indicators_df,by = c("COUNTY","YEAR"), all.y = TRUE) %>%
-  select(-c("ZCTA","HOUSING_UNIT_QUANTITY_CENSUS","ZCTA_POP","COUNTY_POP","ALARM_DATE","CONT_DATE","FIRE_LENGTH_DAYS","FIRE_NAME","PERCENT_ZCTA_BURNED","ZCTA_BY_COUNTY_WEIGHT")) %>%
+  dplyr::select(-c("ZCTA","HOUSING_UNIT_QUANTITY_CENSUS","ZCTA_POP","COUNTY_POP","ALARM_DATE","CONT_DATE","FIRE_LENGTH_DAYS","FIRE_NAME","PERCENT_ZCTA_BURNED","ZCTA_BY_COUNTY_WEIGHT")) %>%
   distinct(.keep_all= TRUE) %>%
   mutate(PERCENT_COUNTY_BURNED_WEIGHTED = coalesce(PERCENT_COUNTY_BURNED_WEIGHTED, 0))
 names(panel_df) <- gsub(" ", "_", names(panel_df))
@@ -178,6 +203,23 @@ panel_df$YEAR <- as.factor(panel_df$YEAR)
 # Average_nonfarm_proprietors_income.Dollars Average_wages_and_salaries.Dollars Employer_contributions_for_employee_pension_and_insurance_funds_5.Thousands_of_dollars
 # Dividends_interest_and_rent_2.Thousands_of_dollars
 
+# Map of the average percent of each county burned in each fire
+mean_percent_df <- weighted_county_percent %>%
+  group_by(COUNTY) %>%
+  summarise(MEAN_PERCENT_COUNTY_BURNED_PER_FIRE = mean(PERCENT_COUNTY_BURNED_WEIGHTED))
+county_pop_df <- percentage_df %>%
+  dplyr::select(COUNTY, COUNTY_POP) %>%
+  distinct(.keep_all= TRUE)
+counties_shapes@data <- merge(counties_shapes@data, mean_percent_df, by.x = "NAME", by.y="COUNTY", all.x=TRUE)
+counties_shapes@data <- merge(counties_shapes@data, county_pop_df, by.x = "NAME", by.y="COUNTY", all.x=TRUE)
+mean_percent_map <- tm_shape(counties_shapes) + tm_fill("MEAN_PERCENT_COUNTY_BURNED_PER_FIRE", 
+                                                        title = "Average Weighted Percent of County Burned per Fire")
+pop_map <- tm_shape(counties_shapes) + tm_fill("COUNTY_POP", 
+                                                        style="quantile",
+                                                        title = "Population per County")
+
+
+# Plots exploring various relationships in the data
 p_zillow <- ggplot(filter(panel_df,PERCENT_COUNTY_BURNED_WEIGHTED!=0), aes(x = PERCENT_COUNTY_BURNED_WEIGHTED, y = ZILLOW_FOR_SALE_LISTINGS_QUANTITY_DEC31)) + 
   geom_point()
 p_average_inc <- ggplot(filter(panel_df,PERCENT_COUNTY_BURNED_WEIGHTED!=0), aes(x = PERCENT_COUNTY_BURNED_WEIGHTED, y = Average_earnings_per_job_dollars.Dollars)) + 
@@ -193,12 +235,20 @@ p_san_bernardino_time <- ggplot(filter(panel_df,COUNTY=="San Bernardino"), aes(x
 p_salary_number_jobs_la <- ggplot(filter(panel_df,COUNTY=="Los Angeles"), aes(x = Wage_and_salary_employment.Number_of_jobs, y = Average_wages_and_salaries.Dollars)) + 
   geom_point()
 p_salary_zillow_la <- ggplot(filter(panel_df,COUNTY=="Los Angeles"), aes(x = Average_wages_and_salaries.Dollars, y = ZILLOW_FOR_SALE_LISTINGS_QUANTITY_DEC31)) + 
-  geom_point()
+  geom_point() +
+  xlab("Average Wages and Salaries ($)") +
+  ylab("Year-End Quantity of Zillow Housing Listings")
+p_YEAR_zillow_la <- ggplot(filter(panel_df,COUNTY=="Los Angeles"), aes(x = YEAR, y = ZILLOW_FOR_SALE_LISTINGS_QUANTITY_DEC31)) + 
+  geom_point() +
+  xlab("Year") +
+  ylab("Year-End Quantity of Zillow Housing Listings")
 
+# Regression testing relationship between a fire's length and the weighted percent of a county burned by it
 fire_length_df <- county_weighted_percentage_df %>%
   filter(!is.na(FIRE_LENGTH_DAYS))
 fire_length_lm <- lm(PERCENT_COUNTY_BURNED_WEIGHTED ~ FIRE_LENGTH_DAYS, fire_length_df)
 
+# Fixed effects regressions testing relationships within the data
 lm_number_jobs_burn <- plm(Wage_and_salary_employment.Number_of_jobs ~ PERCENT_COUNTY_BURNED_WEIGHTED, 
                 data = panel_df, index = c("COUNTY", "YEAR"), model = "within",effect = "twoways")
 lm_salary_zillow <- plm(ZILLOW_FOR_SALE_LISTINGS_QUANTITY_DEC31 ~ Average_wages_and_salaries.Dollars, 
@@ -206,7 +256,21 @@ lm_salary_zillow <- plm(ZILLOW_FOR_SALE_LISTINGS_QUANTITY_DEC31 ~ Average_wages_
 lm_salary_number_jobs <- plm(Average_wages_and_salaries.Dollars ~ Wage_and_salary_employment.Number_of_jobs, 
                              data = panel_df, index = c("COUNTY", "YEAR"), model = "within",effect = "twoways")
 
-#coeftest(lm_number_jobs_burn, vcov = vcovHC, type = "HC1") # see textbook about this
+# LaTeX regression tables for output
+stargazer(fatalities_mod1, fatalities_mod2, fatalities_mod3, 
+          fatalities_mod4, fatalities_mod5, fatalities_mod6, fatalities_mod7, 
+          digits = 3,
+          header = FALSE,
+          type = "latex", 
+          se = rob_se,
+          title = "Linear Panel Regression Models of Traffic Fatalities due to Drunk Driving",
+          model.numbers = FALSE,
+          column.labels = c("(1)", "(2)", "(3)", "(4)", "(5)", "(6)", "(7)"))
+
+# Plots and figures to export
+#mean_percent_map
+#pop_map
+#gridExtra::grid.arrange(p_salary_zillow_la, p_YEAR_zillow_la, ncol=2)
 
 
 # Push everything to GitHub when done:
