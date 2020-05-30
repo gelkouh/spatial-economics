8de9d23af2e059359e3f57f8be233abd0ae39f3e
Graydon "Cedric" Elkouh <gelkouh@uchicago.edu>
2020-05-30 12:37:49 -0400

    Update data.R

--- data.R
+++ data.R
@@ -1,2 +1,3 @@
 ddir <- "/Users/gelkouh/Google Drive (UChicago)/Spatial Economics Cohort (Shared Folder)/Cohort Research Paper/Data"
 # Change this file path to reflect the file path to where the `Data` folder for this project is stored on your machine
+# You can download the data here: https://drive.google.com/file/d/1KuGNe5OkWm5ITNyqeiE4iHAYUZVY0G57/view?usp=sharing
